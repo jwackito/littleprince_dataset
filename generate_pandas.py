7a30cec23079f48b75777794004558b5ce9d77f4
joaquinbogado <joaquinbogado@gmail.com>
2025-08-18 13:36:54 -0300

    Added portuguese to the dataset

--- generate_pandas.py
+++ generate_pandas.py
@@ -10,7 +10,7 @@ def load_littleprince(lang='spanish', count=2314, remove_empty_lines=True, remov
         lines = [line for line in alllines if (line != '\n' and not line.startswith('____') and not line.startswith('-----'))]
     return lines
 
-languages = { lang: [] for lang in ['english', 'spanish', 'french']}
+languages = { lang: [] for lang in ['english', 'spanish', 'french', 'portuguese']}
 
 for lang in languages:
     lines = load_littleprince(lang=lang)
@@ -18,5 +18,6 @@ for lang in languages:
 
 assert len(languages['english']) == len(languages['spanish']), "Las lineas no coinciden entre idiomas eng - esp"
 assert len(languages['spanish']) == len(languages['french']), "Las lineas no coinciden entre idiomas esp - fre"
+assert len(languages['french']) == len(languages['portuguese']), "Las lineas no coinciden entre idiomas fre - por"
 
 pd.DataFrame(languages).to_csv('little_prince_dataset.csv.gz', index=False)
